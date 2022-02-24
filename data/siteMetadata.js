3d01e7c7cd4ddf8c95aee72c4c3578b24aca229c
Robert Turner <robturneris@gmail.com>
2022-02-23 23:09:44 -0500

    Updated site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -15,7 +15,7 @@ const siteMetadata = {
   twitter: 'https://twitter.com/rt3me',
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com/rt3me',
+  linkedin: 'https://www.linkedin.com/in/rt3me',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
