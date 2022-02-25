2375adf2c3474c93d895b7fd6ff1708217f12f8c
Robert Turner <robturneris@gmail.com>
2022-02-24 22:14:10 -0500

    Updated projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,11 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Flip Flop Surf Shop',
+    description: `A full stack React application built on the MERN stack with e-commerce powered
+    by the Stripe API. This site was created as a final project for Nucamp Coding Bootcamp's full
+    stack program.`,
+    imgSrc: '/static/images/project-flip-flop-surf-shop.png',
+    href: 'https://flipflopsurf.shop',
   },
 ]
 
