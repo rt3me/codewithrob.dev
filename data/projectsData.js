399333bee43855042db0ae344a96aa1b66c86dd3
Robert Turner <robturneris@gmail.com>
2022-11-10 22:08:48 -0500

    added howard wangs blog post to projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,6 +8,14 @@ const projectsData = [
     href: '/blog/project-augusta-christian-academy',
   },
   {
+    title: `Howard Wang's Restaurants`,
+    description: `A redesign and new site built for a private group of restaurants with React,
+    Next.js, and Tailwind CSS, the site is deployed to Vercel from GitHub automatically with every
+    new push to the repository.`,
+    imgSrc: '/static/images/project-howard-wangs.jpg',
+    href: '/blog/project-howard-wangs-restaurants',
+  },
+  {
     title: 'Flip Flop Surf Shop',
     description: `A full stack React application built on the MERN stack with e-commerce powered
     by the Stripe API. This site was created as a final project for Nucamp Coding Bootcamp's full
