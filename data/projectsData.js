01538ffff6c0db8e755f85632bd167da7e52aa66
Robert Turner <robturneris@gmail.com>
2022-11-10 21:46:19 -0500

    added acacademy to projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,13 @@
 const projectsData = [
   {
+    title: 'Augusta Christian Academy',
+    description: `A new website built for a school starting in 2023, built with React, Next.js,
+    and Tailwind CSS, the site is deployed to Vercel from GitHub automatically with every new
+    push to the repository.`,
+    imgSrc: '/static/images/project-augusta-christian.jpg',
+    href: '/blog/project-augusta-christian-academy',
+  },
+  {
     title: 'Flip Flop Surf Shop',
     description: `A full stack React application built on the MERN stack with e-commerce powered
     by the Stripe API. This site was created as a final project for Nucamp Coding Bootcamp's full
